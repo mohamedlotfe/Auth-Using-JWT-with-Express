5df492d57bf732f22131d88eec876c870493f46c
Bonaventure Ogeto <bonaogeto1@gmail.com>
2023-10-31 11:17:10 +0300

    user.save does not take a callback

--- controllers/user.js
+++ controllers/user.js
@@ -12,31 +12,33 @@ mongoose
   .catch((err) => console.log(err));
 
 exports.register = async (req, res) => {
+  //Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hasPassword = await bcrypt.hash(req.body.password, salt);
 
-    //Hash password
-    const salt = await bcrypt.genSalt(10);
-    const hasPassword = await bcrypt.hash(req.body.password, salt);
+  // Create an user object
+  let user = new User({
+    email: req.body.email,
+    name: req.body.name,
+    password: hasPassword,
+    user_type_id: req.body.user_type_id,
+  });
 
-    // Create an user object
-    let user = new User({
-        email: req.body.email,
-        name: req.body.name,
-        password: hasPassword,
-        user_type_id: req.body.user_type_id
+  // Save User in the database
+  user
+    .save()
+    .then((registeredUser) => {
+      // create payload then Generate an access token
+      let payload = {
+        id: registeredUser._id,
+        user_type_id: req.body.user_type_id || 0,
+      };
+      const token = jwt.sign(payload, config.TOKEN_SECRET);
+      res.status(200).send({ token });
     })
-
-    // Save User in the database
-    user.save((err, registeredUser) => {
-        if (err) {
-            console.log(err)
-        } else {
-            // create payload then Generate an access token
-            let payload = { id: registeredUser._id, user_type_id: req.body.user_type_id || 0 };
-            const token = jwt.sign(payload, config.TOKEN_SECRET);
-
-            res.status(200).send({ token })
-        }
-    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
 
 exports.login = async (req, res) => {
